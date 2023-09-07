3811810308d3c19386b4fcf8bf69c418bade3f73
Yahia-Ka <yahia.karkori@gmail.com>
2023-09-07 16:18:21 -0700

    Modify 101-quote.c 2nd time

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -6,9 +6,11 @@
  * return: 1 
  *
  */
-int main (void)
+int main(void)
 {
-	int s = strlen("and that piece of art is useful\" - Dora Korpar, 2015-10-19");
-	write(2,"and that piece of art is useful\" - Dora Korpar, 2015-10-19",s);
+	int s = strlen("and that piece of art is useful" - Dora Korpar, 2015-10-19");
+	write(2,
+			"and that piece of art is useful" - Dora Korpar, 2015-10-19",
+			s);
 	return (1);
 }
