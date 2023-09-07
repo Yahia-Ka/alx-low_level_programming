61ce4f12a8ad67297b9b54a797446c739270ee69
Yahia-Ka <yahia.karkori@gmail.com>
2023-09-07 16:08:20 -0700

    Modify File 101-quote.c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,1 +1,14 @@
-!
+#include<stdio.h>
+#include<string.h>
+#include<unistd.h>
+/**
+ * main - main (void)
+ * return: 1 
+ *
+ */
+int main (void)
+{
+	int s = strlen("and that piece of art is useful\" - Dora Korpar, 2015-10-19");
+	write(2,"and that piece of art is useful\" - Dora Korpar, 2015-10-19",s);
+	return (1);
+}
