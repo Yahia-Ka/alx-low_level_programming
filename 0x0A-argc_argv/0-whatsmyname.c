4d8cd7b8fddfc9ba8df3a521153e19fea5cd3169
Yahia-Ka <yahia.karkori@gmail.com>
2023-10-07 18:24:43 +0000

    Modify file 0 and 1

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -1,6 +1,13 @@
 #include "main.h"
 #include <stdio.h>
 
+/**
+ * main - print the file name followed by a new line
+ * @argc: argument count
+ * @argv: arguments passed to main as an array of strings
+ * Return: 0
+ */
+
 
 int main(int argc, char *argv[])
 {
--- 0x0A-argc_argv/1-args.c
+++ 0x0A-argc_argv/1-args.c
@@ -1,6 +1,13 @@
 #include "main.h"
 #include <stdio.h>
 
+/**
+ * main - print the num of args you passed into it followe by a new line
+ * @argc: argument count
+ * @argv: argument vector, array of strings
+ * Return: 0
+ */
+
 int main(int argc, char *argv[])
 {
 	(void) argv;
